ded5a662160a40e9bafc6d1ef51bb09333f2ab3b
Diego Satizabal <diego.satizabal@gmail.com>
2026-04-06 14:15:44 -0300

    New sprites approach

--- src/crosshair.v
+++ src/crosshair.v
@@ -17,62 +17,29 @@ module crosshair (
   localparam [9:0] SPRITE_WIDTH  = 10;
   localparam [9:0] SPRITE_HEIGHT = 10;
 
-  reg [3:0] row;
-  reg [3:0] col;
-
-  function automatic crosshair_pixel;
-    input [3:0] r;
-    input [3:0] c;
-    begin
-      case (r)
-        4'd0: crosshair_pixel = (10'b00_0000_0011 >> c) & 1'b1;
-        4'd1: crosshair_pixel = (10'b00_0000_0011 >> c) & 1'b1;
-        4'd2: crosshair_pixel = (10'b00_0000_0011 >> c) & 1'b1;
-        4'd3: crosshair_pixel = (10'b00_0000_0011 >> c) & 1'b1;
-        4'd4: crosshair_pixel = (10'b00_0000_0011 >> c) & 1'b1;
-        4'd5: crosshair_pixel = (10'b00_0000_0011 >> c) & 1'b1;
-        4'd6: crosshair_pixel = (10'b00_0000_0011 >> c) & 1'b1;
-        4'd7: crosshair_pixel = (10'b00_0000_0011 >> c) & 1'b1;
-        4'd8: crosshair_pixel = (10'b11_1111_1111 >> c) & 1'b1;
-        4'd9: crosshair_pixel = (10'b11_1111_1111 >> c) & 1'b1;
-        default: crosshair_pixel = 1'b0;
-      endcase
-    end
-  endfunction
-
   always @(posedge clk) begin
     if (!rst_n) begin
       active <= 1'b0;
       R <= 2'b00;
       G <= 2'b00;
       B <= 2'b00;
-      row <= 4'd0;
-      col <= 4'd0;
     end else begin
       active <= 1'b0;
       R <= 2'b00;
       G <= 2'b00;
       B <= 2'b00;
-      row <= 4'd0;
-      col <= 4'd0;
 
       // Upper half
       if ((y > (pos_y - SPRITE_HEIGHT)) && (y <= pos_y)) begin
         if ((x > (pos_x - SPRITE_WIDTH)) && (x <= pos_x)) begin
-          row <= pos_y - y;
-          col <= pos_x - x;
-
-          if (crosshair_pixel(pos_y - y, pos_x - x)) begin
+          if (crosshair_pixel(y - pos_y + SPRITE_HEIGHT - 1'b1, pos_x - x)) begin
             active <= 1'b1;
             R <= RGB_Color[5:4];
             G <= RGB_Color[3:2];
             B <= RGB_Color[1:0];
           end
         end else if ((x > pos_x) && (x <= pos_x + SPRITE_WIDTH)) begin
-          row <= pos_y - y;
-          col <= x - pos_x - 1'b1;
-
-          if (crosshair_pixel(pos_y - y, x - pos_x - 1'b1)) begin
+          if (crosshair_pixel(y - pos_y + SPRITE_HEIGHT - 1'b1, x - pos_x)) begin
             active <= 1'b1;
             R <= RGB_Color[5:4];
             G <= RGB_Color[3:2];
@@ -83,20 +50,14 @@ module crosshair (
       // Lower half
       else if ((y > pos_y) && (y <= pos_y + SPRITE_HEIGHT)) begin
         if ((x > (pos_x - SPRITE_WIDTH)) && (x <= pos_x)) begin
-          row <= y - pos_y - 1'b1;
-          col <= pos_x - x;
-
-          if (crosshair_pixel(y - pos_y - 1'b1, pos_x - x)) begin
+          if (crosshair_pixel(pos_y + SPRITE_HEIGHT - y, pos_x - x)) begin
             active <= 1'b1;
             R <= RGB_Color[5:4];
             G <= RGB_Color[3:2];
             B <= RGB_Color[1:0];
           end
         end else if ((x > pos_x) && (x <= pos_x + SPRITE_WIDTH)) begin
-          row <= y - pos_y - 1'b1;
-          col <= x - pos_x - 1'b1;
-
-          if (crosshair_pixel(y - pos_y - 1'b1, x - pos_x - 1'b1)) begin
+          if (crosshair_pixel(pos_y + SPRITE_HEIGHT - y, x - pos_x)) begin
             active <= 1'b1;
             R <= RGB_Color[5:4];
             G <= RGB_Color[3:2];
--- src/explossion.v
+++ src/explossion.v
@@ -1,244 +1,107 @@
 module explosion (
-    input wire rst_n,
-    input wire clk,
-    input wire frames_clk,
-    input wire lines_clk,
-    input wire [9:0] x,
-    input wire [9:0] y,
-    input wire [9:0] pos_x,
-    input wire [9:0] pos_y,
-    input wire fire,
-    input wire [15:0] control,
-    input wire [15:0] my_number,
-    input wire [5:0] RGB_color,
-    output reg active,
-    output reg exploding,
-    output reg [1:0] R,
-    output reg [1:0] G,
-    output reg [1:0] B
+    input  wire       rst_n,
+    input  wire       clk,
+    input  wire       frames_clk,
+    input  wire       lines_clk,
+    input  wire [9:0] x,
+    input  wire [9:0] y,
+    input  wire [9:0] pos_x,
+    input  wire [9:0] pos_y,
+    input  wire       fire,
+    input  wire [15:0] control,
+    input  wire [15:0] my_number,
+    input  wire [5:0] RGB_color,
+    output reg        active,
+    output reg        exploding,
+    output reg [1:0]  R,
+    output reg [1:0]  G,
+    output reg [1:0]  B
 );
+
   localparam [7:0] SPRITE_WIDTH  = 24;
   localparam [7:0] SPRITE_HEIGHT = 24;
 
-  localparam [23:0] SPRITE_5 [23:0] = {
-    24'b0000_0000_0000_0000_0000_1111,
-    24'b0000_0000_0000_0000_1111_1111,
-    24'b0000_0000_0000_0011_1111_1111,
-    24'b0000_0000_0000_1111_1111_1111,
-    24'b0000_0000_0001_1111_1111_1111,
-    24'b0000_0000_0111_1111_1111_1111,
-    24'b0000_0000_1111_1111_1111_1111,
-    24'b0000_0001_1111_1111_1111_1111,
-    24'b0000_0011_1111_1111_1111_1111,
-    24'b0000_0011_1111_1111_1111_1111,
-    24'b0000_0111_1111_1111_1111_1111,
-    24'b0000_1111_1111_1111_1111_1111,
-    24'b0001_1111_1111_1111_1111_1111,
-    24'b0001_1111_1111_1111_1111_1111,
-    24'b0011_1111_1111_1111_1111_1111,
-    24'b0011_1111_1111_1111_1111_1111,
-    24'b0011_1111_1111_1111_1111_1111,
-    24'b0111_1111_1111_1111_1111_1111,
-    24'b0111_1111_1111_1111_1111_1111,
-    24'b1111_1111_1111_1111_1111_1111,
-    24'b1111_1111_1111_1111_1111_1111,
-    24'b1111_1111_1111_1111_1111_1111,
-    24'b1111_1111_1111_1111_1111_1111,
-    24'b1111_1111_1111_1111_1111_1111
-  };
-
-  localparam [23:0] SPRITE_4 [23:0] = {
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0011_1111,
-    24'b0000_0000_0000_0000_1111_1111,
-    24'b0000_0000_0000_0011_1111_1111,
-    24'b0000_0000_0000_1111_1111_1111,
-    24'b0000_0000_0001_1111_1111_1111,
-    24'b0000_0000_0011_1111_1111_1111,
-    24'b0000_0000_0111_1111_1111_1111,
-    24'b0000_0000_0111_1111_1111_1111,
-    24'b0000_0000_1111_1111_1111_1111,
-    24'b0000_0001_1111_1111_1111_1111,
-    24'b0000_0011_1111_1111_1111_1111,
-    24'b0000_0011_1111_1111_1111_1111,
-    24'b0000_0011_1111_1111_1111_1111,
-    24'b0000_0111_1111_1111_1111_1111,
-    24'b0000_0111_1111_1111_1111_1111,
-    24'b0000_0111_1111_1111_1111_1111,
-    24'b0000_1111_1111_1111_1111_1111,
-    24'b0000_1111_1111_1111_1111_1111,
-    24'b0000_1111_1111_1111_1111_1111,
-    24'b0000_1111_1111_1111_1111_1111
-  };
-
-  localparam [23:0] SPRITE_3 [23:0] = {
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_1111,
-    24'b0000_0000_0000_0000_0011_1111,
-    24'b0000_0000_0000_0000_1111_1111,
-    24'b0000_0000_0000_0011_1111_1111,
-    24'b0000_0000_0000_0111_1111_1111,
-    24'b0000_0000_0000_0111_1111_1111,
-    24'b0000_0000_0000_1111_1111_1111,
-    24'b0000_0000_0001_1111_1111_1111,
-    24'b0000_0000_0001_1111_1111_1111,
-    24'b0000_0000_0011_1111_1111_1111,
-    24'b0000_0000_0011_1111_1111_1111,
-    24'b0000_0000_0111_1111_1111_1111,
-    24'b0000_0000_0111_1111_1111_1111,
-    24'b0000_0000_0111_1111_1111_1111,
-    24'b0000_0000_0111_1111_1111_1111
-  };
-
-  localparam [23:0] SPRITE_2 [23:0] = {
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0100,
-    24'b0000_0000_0000_0000_0001_1111,
-    24'b0000_0000_0000_0000_0001_1111,
-    24'b0000_0000_0000_0000_0001_1111,
-    24'b0000_0000_0000_0000_0001_1111,
-    24'b0000_0000_0000_0000_0001_1111,
-    24'b0000_0000_0000_0000_0001_1111,
-    24'b0000_0000_0000_0001_1111_1111,
-    24'b0000_0000_0000_0011_1111_1111,
-    24'b0000_0000_0000_0011_1111_1111,
-    24'b0000_0000_0000_0001_1111_1111
-  };
-
-  localparam [23:0] SPRITE_1 [23:0] = {
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0110,
-    24'b0000_0000_0000_0000_0000_1111,
-    24'b0000_0000_0000_0000_0011_1111,
-    24'b0000_0000_0000_0000_0111_1111,
-    24'b0000_0000_0000_0000_0111_1111,
-    24'b0000_0000_0000_0000_0011_1111
-  };
-
-  localparam [23:0] SPRITE_0 [23:0] = {
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_0000,
-    24'b0000_0000_0000_0000_0000_1111,
-    24'b0000_0000_0000_0000_0000_1111,
-    24'b0000_0000_0000_0000_0000_1111,
-    24'b0000_0000_0000_0000_0000_1111
-  };
-
-  localparam SPRITES_COUNT = 4'b0101;
-  localparam FRAMES_DELAY  = 16'h0960;
+  localparam [3:0] SPRITES_COUNT = 4'd5;
+  localparam [15:0] FRAMES_DELAY = 16'h0960;
 
   reg [9:0] my_x;
   reg [9:0] my_y;
   reg [3:0] counter;
   reg [15:0] frames_counter;
   reg [1:0] direction;
-  reg current_pixel;
   reg explode;
 
-  assign exploding = explode;
-
   always @(posedge lines_clk) begin
-    if (~rst_n) begin
-      active <= 0;
-      frames_counter <= 0;
-      counter <= 0;
-      direction <= 0;
-      explode <= 0;
+    if (!rst_n) begin
+      active <= 1'b0;
+      frames_counter <= 16'd0;
+      counter <= 4'd0;
+      direction <= 2'd0;
+      explode <= 1'b0;
+      exploding <= 1'b0;
+      my_x <= 10'd0;
+      my_y <= 10'd0;
     end else begin
+      exploding <= explode;
+
       if (my_number[0] && !control[0]) begin
         if (fire && !explode) begin
-          explode <= 1;
+          explode <= 1'b1;
+          exploding <= 1'b1;
           my_x <= pos_x;
           my_y <= pos_y;
+          counter <= 4'd0;
+          direction <= 2'd0;
+          frames_counter <= 16'd0;
         end
       end
 
       if (my_number[1] && control[0] && !control[1]) begin
         if (fire && !explode) begin
-          explode <= 1;
+          explode <= 1'b1;
+          exploding <= 1'b1;
           my_x <= pos_x;
           my_y <= pos_y;
+          counter <= 4'd0;
+          direction <= 2'd0;
+          frames_counter <= 16'd0;
         end
       end
 
       if (my_number[2] && control[0] && control[1] && !control[2]) begin
         if (fire && !explode) begin
-          explode <= 1;
+          explode <= 1'b1;
+          exploding <= 1'b1;
           my_x <= pos_x;
           my_y <= pos_y;
+          counter <= 4'd0;
+          direction <= 2'd0;
+          frames_counter <= 16'd0;
         end
       end
 
       if (my_number[3] && control[0] && control[1] && control[2] && !control[3]) begin
         if (fire && !explode) begin
-          explode <= 1;
+          explode <= 1'b1;
+          exploding <= 1'b1;
           my_x <= pos_x;
           my_y <= pos_y;
+          counter <= 4'd0;
+          direction <= 2'd0;
+          frames_counter <= 16'd0;
         end
       end
     end
   end
 
   always @(posedge lines_clk) begin
-    if (explode) begin
+    if (!rst_n) begin
+      frames_counter <= 16'd0;
+      counter <= 4'd0;
+      direction <= 2'd0;
+      explode <= 1'b0;
+      exploding <= 1'b0;
+    end else if (explode) begin
       if (frames_counter + 1'b1 < FRAMES_DELAY) begin
         frames_counter <= frames_counter + 1'b1;
       end else begin
@@ -250,110 +113,92 @@ module explosion (
         end else begin
           if (counter - 1'b1 == 0) begin
             direction <= direction + 1'b1;
-            explode <= 0;
+            explode <= 1'b0;
+            exploding <= 1'b0;
           end
           counter <= counter - 1'b1;
         end
-        frames_counter <= 0;
+        frames_counter <= 16'd0;
       end
     end
   end
 
   always @(posedge clk) begin
-    if (~rst_n) begin
-      active <= 0;
-      my_x <= 0;
-      my_y <= 0;
+    if (!rst_n) begin
+      active <= 1'b0;
+      R <= 2'b00;
+      G <= 2'b00;
+      B <= 2'b00;
     end else begin
+      active <= 1'b0;
+      R <= 2'b00;
+      G <= 2'b00;
+      B <= 2'b00;
+
       if (explode) begin
+        // upper half
         if ((y > (my_y - SPRITE_HEIGHT)) && (y <= my_y)) begin
-          if ((x > (my_x - SPRITE_HEIGHT)) && (x <= my_x)) begin
-            active <= 1;
-
-            if (counter == 0)       current_pixel <= SPRITE_0[my_y - y][(my_x - x)];
-            else if (counter == 1)  current_pixel <= SPRITE_1[my_y - y][(my_x - x)];
-            else if (counter == 2)  current_pixel <= SPRITE_2[my_y - y][(my_x - x)];
-            else if (counter == 3)  current_pixel <= SPRITE_3[my_y - y][(my_x - x)];
-            else if (counter == 4)  current_pixel <= SPRITE_4[my_y - y][(my_x - x)];
-            else if (counter == 5)  current_pixel <= SPRITE_5[my_y - y][(my_x - x)];
-            else                    counter <= 0;
-
-            if (current_pixel == 1'b1) begin
+          if ((x > (my_x - SPRITE_WIDTH)) && (x <= my_x)) begin
+            if (
+              (counter == 4'd0 && sprite_0_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, my_x - x)) ||
+              (counter == 4'd1 && sprite_1_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, my_x - x)) ||
+              (counter == 4'd2 && sprite_2_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, my_x - x)) ||
+              (counter == 4'd3 && sprite_3_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, my_x - x)) ||
+              (counter == 4'd4 && sprite_4_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, my_x - x)) ||
+              (counter == 4'd5 && sprite_5_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, my_x - x))
+            ) begin
+              active <= 1'b1;
               R <= RGB_color[5:4];
               G <= RGB_color[3:2];
               B <= RGB_color[1:0];
-            end else begin
-              active <= 0; R <= 2'b00; G <= 2'b00; B <= 2'b11;
             end
-
           end else if ((x > my_x) && (x <= my_x + SPRITE_WIDTH)) begin
-            active <= 1;
-
-            if (counter == 0)       current_pixel <= SPRITE_0[my_y - y][x - my_x - 1];
-            else if (counter == 1)  current_pixel <= SPRITE_1[my_y - y][x - my_x - 1];
-            else if (counter == 2)  current_pixel <= SPRITE_2[my_y - y][x - my_x - 1];
-            else if (counter == 3)  current_pixel <= SPRITE_3[my_y - y][x - my_x - 1];
-            else if (counter == 4)  current_pixel <= SPRITE_4[my_y - y][x - my_x - 1];
-            else if (counter == 5)  current_pixel <= SPRITE_5[my_y - y][x - my_x - 1];
-            else                    counter <= 0;
-
-            if (current_pixel == 1'b1) begin
+            if (
+              (counter == 4'd0 && sprite_0_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, x - my_x - 1'b1)) ||
+              (counter == 4'd1 && sprite_1_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, x - my_x - 1'b1)) ||
+              (counter == 4'd2 && sprite_2_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, x - my_x - 1'b1)) ||
+              (counter == 4'd3 && sprite_3_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, x - my_x - 1'b1)) ||
+              (counter == 4'd4 && sprite_4_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, x - my_x - 1'b1)) ||
+              (counter == 4'd5 && sprite_5_pixel(y - my_y + SPRITE_HEIGHT - 1'b1, x - my_x - 1'b1))
+            ) begin
+              active <= 1'b1;
               R <= RGB_color[5:4];
               G <= RGB_color[3:2];
               B <= RGB_color[1:0];
-            end else begin
-              active <= 0; R <= 2'b00; G <= 2'b00; B <= 2'b11;
             end
-
-          end else begin
-            active <= 0;
           end
-
-        end else if ((y > my_y) && (y <= my_y + SPRITE_HEIGHT)) begin
-          if ((x > (my_x - SPRITE_HEIGHT)) && (x <= my_x)) begin
-            active <= 1;
-
-            if (counter == 0)       current_pixel <= SPRITE_0[y - my_y - 1][(my_x - x)];
-            else if (counter == 1)  current_pixel <= SPRITE_1[y - my_y - 1][(my_x - x)];
-            else if (counter == 2)  current_pixel <= SPRITE_2[y - my_y - 1][(my_x - x)];
-            else if (counter == 3)  current_pixel <= SPRITE_3[y - my_y - 1][(my_x - x)];
-            else if (counter == 4)  current_pixel <= SPRITE_4[y - my_y - 1][(my_x - x)];
-            else if (counter == 5)  current_pixel <= SPRITE_5[y - my_y - 1][(my_x - x)];
-            else                    counter <= 0;
-
-            if (current_pixel == 1'b1) begin
+        end
+        // lower half
+        else if ((y > my_y) && (y <= my_y + SPRITE_HEIGHT)) begin
+          if ((x > (my_x - SPRITE_WIDTH)) && (x <= my_x)) begin
+            if (
+              (counter == 4'd0 && sprite_0_pixel(my_y + SPRITE_HEIGHT - y, my_x - x)) ||
+              (counter == 4'd1 && sprite_1_pixel(my_y + SPRITE_HEIGHT - y, my_x - x)) ||
+              (counter == 4'd2 && sprite_2_pixel(my_y + SPRITE_HEIGHT - y, my_x - x)) ||
+              (counter == 4'd3 && sprite_3_pixel(my_y + SPRITE_HEIGHT - y, my_x - x)) ||
+              (counter == 4'd4 && sprite_4_pixel(my_y + SPRITE_HEIGHT - y, my_x - x)) ||
+              (counter == 4'd5 && sprite_5_pixel(my_y + SPRITE_HEIGHT - y, my_x - x))
+            ) begin
+              active <= 1'b1;
               R <= RGB_color[5:4];
               G <= RGB_color[3:2];
               B <= RGB_color[1:0];
-            end else begin
-              active <= 0; R <= 2'b00; G <= 2'b00; B <= 2'b11;
             end
-
           end else if ((x > my_x) && (x <= my_x + SPRITE_WIDTH)) begin
-            active <= 1;
-
-            if (counter == 0)       current_pixel <= SPRITE_0[y - my_y - 1][x - my_x - 1];
-            else if (counter == 1)  current_pixel <= SPRITE_1[y - my_y - 1][x - my_x - 1];
-            else if (counter == 2)  current_pixel <= SPRITE_2[y - my_y - 1][x - my_x - 1];
-            else if (counter == 3)  current_pixel <= SPRITE_3[y - my_y - 1][x - my_x - 1];
-            else if (counter == 4)  current_pixel <= SPRITE_4[y - my_y - 1][x - my_x - 1];
-            else if (counter == 5)  current_pixel <= SPRITE_5[y - my_y - 1][x - my_x - 1];
-            else                    counter <= 0;
-
-            if (current_pixel == 1'b1) begin
+            if (
+              (counter == 4'd0 && sprite_0_pixel(my_y + SPRITE_HEIGHT - y, x - my_x - 1'b1)) ||
+              (counter == 4'd1 && sprite_1_pixel(my_y + SPRITE_HEIGHT - y, x - my_x - 1'b1)) ||
+              (counter == 4'd2 && sprite_2_pixel(my_y + SPRITE_HEIGHT - y, x - my_x - 1'b1)) ||
+              (counter == 4'd3 && sprite_3_pixel(my_y + SPRITE_HEIGHT - y, x - my_x - 1'b1)) ||
+              (counter == 4'd4 && sprite_4_pixel(my_y + SPRITE_HEIGHT - y, x - my_x - 1'b1)) ||
+              (counter == 4'd5 && sprite_5_pixel(my_y + SPRITE_HEIGHT - y, x - my_x - 1'b1))
+            ) begin
+              active <= 1'b1;
               R <= RGB_color[5:4];
               G <= RGB_color[3:2];
               B <= RGB_color[1:0];
-            end else begin
-              active <= 0; R <= 2'b00; G <= 2'b00; B <= 2'b11;
             end
-
-          end else begin
-            active <= 0;
           end
-
-        end else begin
-          active <= 0;
         end
       end
     end
